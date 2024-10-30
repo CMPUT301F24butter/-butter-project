908c3f339df3f76c146f396907ba7497eeffa9bb
Arsalan Firoozkoohi <181882511+arsalan-firoozkoohi@users.noreply.github.com>
2024-10-29 18:05:19 -0600

    Made changes to EventsFragment, Added RegisteredFragment+ViewPagerAdapter+WaitingListAdapter+RegisteredFragment

---
+++ app/src/main/java/com/example/butter/RegisteredFragment.java
@@ -0,0 +1,27 @@
+package com.example.butter;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.example.butter.EventsFragment;
+
+public class RegisteredFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_registered, container, false);
+        ListView registeredList = view.findViewById(R.id.registeredList);
+
+        // Set up your ListView adapter and data here
+
+        return view;
+    }
+}

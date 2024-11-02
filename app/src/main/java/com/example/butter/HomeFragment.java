4cbe44e2319927270b89b99b4a7e0a7cca8c6ef3
Angela Dakay <angdkay@gmail.com>
2024-11-01 23:23:14 -0600

    Added Admins Side (WIP) + home screen changes
    Admin XML and home screen changes based off role.

--- app/src/main/java/com/example/butter/HomeFragment.java
+++ app/src/main/java/com/example/butter/HomeFragment.java
@@ -1,12 +1,31 @@
 package com.example.butter;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.HorizontalScrollView;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,8 +43,22 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    // Need access to all events, users, and posters
+    private FirebaseFirestore db;
+    private CollectionReference eventRef;
+    private CollectionReference userRef;
+
+    String deviceID;
+
+    // Lists of events, users, and posters
+    private ArrayList<Event> eventsList;
+    private ArrayList<User> usersList;
+
     public HomeFragment() {
-        // Required empty public constructor
+        eventsList = new ArrayList<>();
+        db = FirebaseFirestore.getInstance();
+        eventRef = db.collection("event"); // event collection
+        userRef = db.collection("user");
     }
 
     /**
@@ -53,12 +86,61 @@ public class HomeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        Bundle args = getArguments();
+        deviceID = args.getString("deviceID");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // get access to elements in the xml
+        TextView greetingText = view.findViewById(R.id.greetingText);
+        TextView qrCodeText = view.findViewById(R.id.qrCodeText);
+        TextView upcomingText = view.findViewById(R.id.upcomingText);
+        HorizontalScrollView upcomingScrollView = view.findViewById(R.id.horizontalScrollView);
+        TextView waitingText = view.findViewById(R.id.waiting_list_label);
+        HorizontalScrollView waitingScrollView = view.findViewById(R.id.waitingListScrollView);
+
+        RelativeLayout adminSpinnerLayout = view.findViewById(R.id.admin_spinner_layout);
+        Spinner adminSpinner = view.findViewById(R.id.entrants_spinner);
+        RecyclerView adminRecyclerView = view.findViewById(R.id.admin_recycler_view);
+
+        // Populating the spinner
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(
+                requireContext(), android.R.layout.simple_spinner_item, new String[]{"Events", "Profiles", "Images", "Entrants Page"});
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adminSpinner.setAdapter(spinnerAdapter);
+
+        // depending on users privileges, user sees either browsing or upcoming/waiting list
+        userRef.document(deviceID).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot doc = task.getResult();
+                    if (doc.exists()) {
+                        String privileges = doc.getString("userInfo.privilegesString");
+
+                        // If user is an admin / admin + organizer, they get browse abilities
+                        if (Objects.equals(privileges, "400") || Objects.equals(privileges, "600")) {
+                            adminSpinnerLayout.setVisibility(View.VISIBLE);
+                            adminRecyclerView.setVisibility(View.VISIBLE);
+
+                        // If user is an entrant / entrant + organizer, only has upcoming / waiting list
+                        } else if (Objects.equals(privileges, "100") || Objects.equals(privileges, "300")) {
+                            greetingText.setVisibility(View.VISIBLE);
+                            qrCodeText.setVisibility(View.VISIBLE);
+                            upcomingText.setVisibility(View.VISIBLE);
+                            upcomingScrollView.setVisibility(View.VISIBLE);
+                            waitingText.setVisibility(View.VISIBLE);
+                            waitingScrollView.setVisibility(View.VISIBLE);
+                        }
+                    }
+                }
+            }
+        });
+        return view;
     }
 }
